6e4ef116e7130c62ed9d5bd7cf97e09f541b73a3
dnchia <daniel.chia.95@gmail.com>
2017-03-06 16:07:37 +0100

    Introduced Bootstrap 3.3.7 in Angular2 project. --> http://valor-software.com/ng2-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ProgressbarModule } from 'ng2-bootstrap/progressbar';
 
 import { AppComponent } from './app.component';
+import { LoginPanelComponent } from './components/login-panel/login-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPanelComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ProgressbarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
