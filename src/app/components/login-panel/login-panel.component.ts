805fff64905c0025b056107ab1e833b983ed63ac
francisco <al286321@uji.es>
2017-03-08 19:40:59 +0100

    implemented loginPanel

--- src/app/components/login-panel/login-panel.component.ts
+++ src/app/components/login-panel/login-panel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, Validators, FormGroup, AbstractControl} from "@angular/forms";
+import {UserService} from "../../services/UserService";
 
 @Component({
   selector: 'login-panel',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPanelComponent implements OnInit {
 
-  constructor() { }
+
+  private form : FormGroup;
+
+  private userInput: AbstractControl;
+  private username: string = '';
+
+  private passwordInput: AbstractControl;
+  private password: string = '';
+
+  constructor(public formBuilder: FormBuilder, public userService: UserService) {
+
+    this.form = this.formBuilder.group({
+      user: new FormControl('', Validators.compose([Validators.required])),
+      password: new FormControl('', Validators.compose([Validators.required])),
+    });
+
+    this.userInput = this.form.controls['user'];
+    this.passwordInput = this.form.controls['password'];
+  }
 
   ngOnInit() {
+  }
+
+  public onSubmitLogin() {
+    //TODO: Manage register
+    console.log("Submit del login enviado");
   }
 
 }
