276a7de24eb748a2513e985fed5b77834653c225
dnchia <daniel.chia.95@gmail.com>
2017-04-03 18:48:01 +0200

    Refactor done in UserService & the update user method. All finished, but not tested against the server.

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -2,57 +2,88 @@ import {Http, Response} from "@angular/http";
 import {Injectable} from "@angular/core";
 import {StorageService} from "./StorageService";
 import {User} from "../models/User";
+import {ServerConfig} from "../environment/Server.config";
 
 @Injectable()
 export class UserService {
 
-  public constructor(private http: Http, private storage: StorageService) { }
-
+  private serverConfig : ServerConfig;
   //FIXME: Include real directions
-  private registerUserRoute = '';
-  private loginUserRoute = '';
+  private REGISTER_USER_DIRECTION = '/api-v1/signup/supervisor';
+  private LOGIN_USER_DIRECTION = '';
+  private UPDATE_USER_DIRECTION ='';
+
+
+  public constructor(private http: Http, private storage: StorageService) {
+    this.serverConfig = this.storage.getServerConfig();
+  }
+
+
+  /** Actions */
 
   public doLogin(username: string, password: string) : boolean {
-    let loginOK = false;
-    let data = {};
+    let serverUrl : string = `${this.serverConfig.secure ? 'https://' : 'http://'}${this.serverConfig.host}:${this.serverConfig.port}${this.LOGIN_USER_DIRECTION}`;
+    let body = {username, password};
+    let OK : boolean;
+    let data;
 
-    this.http.post(this.loginUserRoute, {username, password}).subscribe(
+    this.http.post(serverUrl, body).subscribe(
       (response: Response) => {
-        loginOK = response.ok;
+        OK = response.ok;
         data = response.json();
       },
-      (err) => {},
+      (err) => {OK = false},
       () => {}
     );
 
-    if (loginOK) {
+    if (OK) {
       this.storage.saveUserFromLogIn(this.buildUser(data));
       return true;
     }
     return false;
   }
 
+  public registerUser(username: string, password: string, email: string) : boolean {
+    let serverUrl : string = `${this.serverConfig.secure ? 'https://' : 'http://'}${this.serverConfig.host}:${this.serverConfig.port}${this.REGISTER_USER_DIRECTION}`;
+    let body = {username, password, email};
+    let OK : boolean;
+
+    this.http.post(serverUrl, body).subscribe(
+      (userOK : Response) => {OK = true},
+      (err) => {OK = false},
+      () => {}
+    );
+
+    return OK;
+  }
+
+  public updateUser(email: string, newPassword: string) : boolean {
+    let serverUrl : string = `${this.serverConfig.secure ? 'https://' : 'http://'}${this.serverConfig.host}:${this.serverConfig.port}${this.UPDATE_USER_DIRECTION}`;
+    let body = {email, newPassword};
+    let OK : boolean;
+    let data;
+
+    this.http.put(serverUrl, body).subscribe(
+      (transactionOK : Response) => {OK = true; data = transactionOK.json()},
+      (err) => {OK = false},
+      () => {}
+    );
+
+    if (OK) {
+      let newUser = this.buildUser(data);
+      this.storage.saveUserFromLogIn(newUser);
+    }
+
+    return OK;
+  }
+
+
+  /** Utility */
+
   //FIXME: Build real user
   private buildUser(data : any) : User {
     let user = new User();
     user.name = "Prueba";
     return user;
-  }
-
-  public registerUser(username: string, password: string, email: string) : boolean {
-    let waitingToServer = true;
-    let data = {username, password, email};
-    let correctRegistering = false;
-
-    this.http.post(this.registerUserRoute, data).map((userOK : Response) => {
-      waitingToServer = false;
-      correctRegistering = userOK.ok;
-    });
-    return correctRegistering;
-  }
-
-  public updateUser(email: string, newPassword: string) : boolean {
-    //FIXME: Complete
-    return false;
   }
 }
