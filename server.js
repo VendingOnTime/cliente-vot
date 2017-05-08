1a0791476a4a964ccf08829f9796d9afaa6f7825
dnchia <daniel.chia.95@gmail.com>
2017-05-08 18:22:05 +0200

    Updated package.json, changed travis.yml

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/dist'));
 
-//const forceSSL = function() {
-//  return function (req, res, next) {
-//    if (req.headers['x-forwarded-proto'] !== 'https') {
-//      return res.redirect(['https://', req.get('Host'), req.url].join(''));
-//    }
-//  }
-//};
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(['https://', req.get('Host'), req.url].join(''));
+    }
+  }
+};
 
-//app.use(forceSSL());
+app.use(forceSSL());
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
